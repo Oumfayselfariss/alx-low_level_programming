eee56f91e2dffb2c0e12bdb4f277aa404213e7d6
Oumfayselfariss <fayselfariss22@gmail.com>
2023-05-04 06:37:02 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,26 @@
-#include "main.h"
-
 /**
  * rot13 - encodes a string using rot13
+ * @s: string to encode
  *
- * @s: string to be encoded
- *
- * Return: pointer to the encoded string
+ * Return: pointer to encoded string
  */
 char *rot13(char *s)
 {
-        int i, j;
-        char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  char *p = s;
+  int i;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; alphabets[j] != '\0'; j++)
-                {
-                        if (s[i] == alphabets[j])
-                        {
-                                s[i] = rot13[j];
-                                break;
-                        }
-                }
-        }
+  while (*p)
+  {
+    for (i = 0; (*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z'); i++)
+    {
+      if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+        *p += 13;
+      else
+        *p -= 13;
+      p++;
+    }
+    p++;
+  }
 
-        return s;
+  return s;
 }
