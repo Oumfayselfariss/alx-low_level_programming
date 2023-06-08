7d9a14996ea049c273b8d52dae99ef93c11e1dde
Oumfayselfariss <fayselfariss22@gmail.com>
2023-06-08 05:42:08 -0700

    code correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,34 @@
-#include <string>
-
-unsigned int binary_to_uint(const std::string& b)
+i#include "main.h"
+/**
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: binary number
+  * Return: the converted number or 0 if it fails
+  */
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned int uint = 0;
-    unsigned int place = 1;
+	unsigned int uint, place;
+	int n;
 
-    for (int n = b.length() - 1; n >= 0; n--)
-    {
-        if (b[n] == '0')
-        {
-            place *= 2;
-        }
-        else if (b[n] == '1')
-        {
-            uint += place;
-            place *= 2;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+	if (!b)
+		return (0);
 
-    return uint;
+	for (n = 0; b[n]; n++)
+		;
+	n--;
+	for (place = 1, uint = 0; n >= 0; n--)
+	{
+		if (b[n] == '0')
+		{
+			place *= 2;
+			continue;
+		}
+		else if (b[n] == '1')
+		{
+			uint += place;
+			place *= 2;
+			continue;
+		}
+		return (0);
+	}
+	return (uint);
 }
